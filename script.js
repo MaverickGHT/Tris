3c5af5cbdcd30f5abba8edec700996236af29bc8
Gabriele <cregg27@gmail.com>
2022-09-28 14:39:03 +0200

    versione preliminare

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 const prompt= require ('prompt-sync')();
 
+let inizio = Math.round(Math.random());
+let giocatore1 = false;
+let giocatore2 = false;
+let turno = 1;
+
+if (inizio==0) {
+    giocatore1=true;
+    console.log("Inizia il giocatore 1");
+} else {
+    giocatore2=true;
+    console.log("Inizia il giocatore 2");
+}
+
+if (turno%2==0) {
+    
+}
